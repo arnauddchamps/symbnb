64e218556abacf36bac19f540b362b40076a9ea5
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2019-05-28 16:00:37 +0200

    Twig : Routes paramétrées & routes multiples des adresses dynamiques

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class HomeController extends Controller {
 
    /**
+    * @Route("/hello/{prenom}/age/{age}", name="hello")
+    * @Route("/hello")
+    * @Route("/hello/{prenom}")
+    * Montre la page qui dit bonjour
+    * 
+    * @return void
+    */
+   public function hello($prenom = "anonyme", $age = 0) {
+      return new Response("Bonjour " . $prenom . " vous avez " .  $age . " ans");
+   }
+
+   /**
     * @Route("/", name="homepage")
     */
    public function home(){
