227903a2785a4a3ee36c1e906c8b4e4dfb2a5948
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2019-05-29 10:40:53 +0200

    Slugify : slug facilement composer

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -13,17 +14,19 @@ class AppFixtures extends Fixture
     {
 
         $faker = Factory::create('FR-fr');
+        $slugify = new Slugify();
 
         for($i = 1; $i <= 30; $i++){
             $ad = new Ad;
 
             $title = $faker->sentence(6);
+            $slug = $slugify->slugify($title);
             $coverImage = $faker->imageUrl(1000,350);
             $introduction = $faker->paragraph(2);
             $content = '<p>' . join('<p></p>',$faker->paragraphs(5)) . '</p>';
 
             $ad->setTitle($title)
-                ->setSlug("titre-de-l-annonce-n-$i")
+                ->setSlug($slug)
                 ->setCoverImage($coverImage)
                 ->setIntroduction($introduction)
                 ->setContent($content)
