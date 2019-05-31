5ae70c08b1d31b72ef292391918ee319876ef467
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2019-05-31 11:26:35 +0200

    Configurer les champs de formulaires

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -6,20 +6,42 @@ use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AdType extends AbstractType
 {
+
+
+    /**
+     * Permet d'avoir la configuration de base d'un champ
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder) {
+        return [
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+            ];
+    }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('price')
-            ->add('introduction')
-            ->add('content')
-            ->add('coverImage')
-            ->add('rooms')
-        ;
+            ->add('title', TextType::class, $this->getConfiguration("Titre","Tapez un titre pour votre annonce"))
+            ->add('slug', TextType::class, $this->getConfiguration("Chaine URL","Adresse web (automatique)"))
+            ->add('coverImage', UrlType::class, $this->getConfiguration("Url de l'image principale","Donnez l'adresse d'une image"))
+            ->add('introduction', TextType::class, $this->getConfiguration("Introduction","Tapez une intro"))
+            ->add('content', TextareaType::class, $this->getConfiguration("Description","Tapez une description à l'annonce"))
+            ->add('rooms', IntegerType::class, $this->getConfiguration("Nombre de chambres","vous avez cb de chambres ?"))
+            ->add('price', MoneyType::class, $this->getConfiguration("Prix par nuit","Saisir le prix de la nuitée"))
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
