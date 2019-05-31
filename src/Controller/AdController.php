d8ec17df7a1b7c671a21558a922f28348902feba
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2019-05-31 14:56:22 +0200

    56 - Créer des sous-formulaires avec les CollectionType.mp4

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Ad;
 use App\Form\AdType;
+use App\Entity\Image;
 use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -35,7 +36,14 @@ class AdController extends AbstractController
      */
     public function create(Request $request, ObjectManager $manager){
         $ad = new Ad();
-        
+
+        $image = new Image();
+
+        $image->setUrl('http://placehold.it/400x200')
+                ->setCaption('Titre 1');
+
+        $ad->addImage($image);
+    
         $form = $this->createForm(AdType::class, $ad);
 
         $form->handleRequest($request);
--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Ad;
+use App\Form\ImageType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -11,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class AdType extends AbstractType
 {
@@ -34,14 +36,42 @@ class AdType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class, $this->getConfiguration("Titre","Tapez un titre pour votre annonce"))
-            ->add('slug', TextType::class, $this->getConfiguration("Chaine URL","Adresse web (automatique)"))
-            ->add('coverImage', UrlType::class, $this->getConfiguration("Url de l'image principale","Donnez l'adresse d'une image"))
-            ->add('introduction', TextType::class, $this->getConfiguration("Introduction","Tapez une intro"))
-            ->add('content', TextareaType::class, $this->getConfiguration("Description","Tapez une description à l'annonce"))
-            ->add('rooms', IntegerType::class, $this->getConfiguration("Nombre de chambres","vous avez cb de chambres ?"))
-            ->add('price', MoneyType::class, $this->getConfiguration("Prix par nuit","Saisir le prix de la nuitée"))
-            ;
+            ->add(
+                'title', 
+                TextType::class, 
+                $this->getConfiguration("Titre","Tapez un titre pour votre annonce")
+            )
+            ->add('slug', 
+                TextType::class, 
+                $this->getConfiguration("Chaine URL","Adresse web (automatique)")
+            )
+            ->add('coverImage', 
+                UrlType::class, 
+                $this->getConfiguration("Url de l'image principale","Donnez l'adresse d'une image")
+            )
+            ->add('introduction', 
+                TextType::class, 
+                $this->getConfiguration("Introduction","Tapez une intro")
+            )
+            ->add('content', 
+                TextareaType::class, 
+                $this->getConfiguration("Description","Tapez une description à l'annonce")
+            )
+            ->add('rooms', 
+                IntegerType::class, 
+                $this->getConfiguration("Nombre de chambres","vous avez cb de chambres ?")
+            )
+            ->add('price', 
+                MoneyType::class, 
+                $this->getConfiguration("Prix par nuit","Saisir le prix de la nuitée")
+            )
+            ->add('images',
+                CollectionType::class,
+                [ 
+                    'entry_type' => ImageType::class
+                ]
+            )
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
