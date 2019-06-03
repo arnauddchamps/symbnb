13913481614aca5d233962ae22faa18de8f47e10
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2019-06-03 15:37:31 +0200

    Créer le formulaire d'inscription lee RegistrationType

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -37,5 +39,22 @@ class AccountController extends AbstractController
      */
     public function logout() {
         // rien
+    }
+
+    /**
+     * Permet d'afficher le form inscription
+     * 
+     * @Route("/register", name="account_register")
+     *
+     * @return Response
+     */
+    public function register(){
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        return $this->render('account/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
