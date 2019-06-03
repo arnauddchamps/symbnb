ea036dd590363d1aca574431eb284f771ed232cc
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2019-06-03 15:15:11 +0200

    Authentification: permettre à l'utilisateur de se déconnecter

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,16 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccountController extends AbstractController
 {
     /**
+     * Permet d'afficher et de gérer le form de connexion
+     * 
      * @Route("/login", name="account_login")
+     * 
+     * @return Response
      */
     public function login()
     {
         return $this->render('account/login.html.twig');
+    }
+
+    /**
+     * Permet de se déconnecter
+     *
+     * @Route("/logout", name="account_logout")
+     * 
+     * @return void
+     */
+    public function logout() {
+        // rien
     }
 }
