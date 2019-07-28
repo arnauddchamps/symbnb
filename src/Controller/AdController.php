b65cd3bfc211292ae1b064d005d4dc6a23466991
arnauddchamps <36513985+arnauddchamps@users.noreply.github.com>
2019-07-28 14:44:04 +0200

    101 - Récupération des rôles dans l'entité User

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -11,6 +11,8 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -31,6 +33,7 @@ class AdController extends AbstractController
      * Permet de créer une annonce
      * 
      * @Route("ads/new", name="ads_create")
+     * @IsGranted("ROLE_USER")
      * 
      * @return Response
      */
@@ -74,6 +77,7 @@ class AdController extends AbstractController
      * Permet d'afficher le formulaire d'édition
      * 
      * @Route("ads/{slug}/edit", name="ads_edit")
+     * @Security("is_granted('ROLE_USER') and user === ad.getAuthor()", message="Cette annonce ne vous appartient pas !!")
      *
      * @return Response
      */
