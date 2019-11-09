b5d25312108df56633bec9ebd944bf0b38600343
arnauddchamps <36513985+arnauddchamps@users.noreply.github.com>
2019-11-09 12:11:25 +0100

    125 - Utiliser un DataTransformer pour gérer les dates au format français

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -8,23 +8,32 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Form\DataTransformer\FrenchToDateTimeTransformer;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BookingType extends ApplicationType
 {
+    private $transformer;
+
+    public function __construct(FrenchToDateTimeTransformer $transformer)
+    {
+        $this->transformer = $transformer;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('startDate', DateType::class, $this->getConfiguration("Date d'arrivée", "La date d'arrivée", [ 'label' => 'Date de début',
-                'widget' => 'single_text',
-                'html5' => false,
+            ->add('startDate', TextType::class, $this->getConfiguration("Date d'arrivée", "La date d'arrivée", [ 'label' => 'Date de début',
                 'attr' => ['class' => 'datepicker']]))
-            ->add('endDate', DateType::class, $this->getConfiguration("Date de départ", "La date de départ", [ 'label' => 'Date de fin',
-                'widget' => 'single_text',
-                'html5' => false,
+            ->add('endDate', TextType::class, $this->getConfiguration("Date de départ", "La date de départ", [ 'label' => 'Date de fin',
                 'attr' => ['class' => 'datepicker']]))
             ->add('comment', TextareaType::class, $this->getConfiguration(false, "Si vous avez go", ['required' =>false]))
         ;
+
+        $builder->get('startDate')->addModelTransformer($this->transformer);
+        $builder->get('endDate')->addModelTransformer($this->transformer);
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
