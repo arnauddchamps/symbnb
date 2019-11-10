ad804020a385ec47a4d373bb699080b93a6da6c6
arnauddchamps <36513985+arnauddchamps@users.noreply.github.com>
2019-11-10 16:10:51 +0100

    158 - Création du formulaire d'édition des annonces

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -16,6 +20,33 @@ class AdminAdController extends AbstractController
         
         return $this->render('admin/ad/index.html.twig', [
             'ads' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * Permet d'afficher le form d'édition
+     * 
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit")
+     */
+    public function edit(Ad $ad, Request $request, ObjectManager $manager){
+
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce a bien été enregistrée"
+            );
+        }
+
+        return $this->render('admin/ad/edit.html.twig', [
+            "ad" => $ad,
+            "form" => $form->createView()
         ]);
     }
 }
