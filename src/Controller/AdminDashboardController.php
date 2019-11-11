5c9fd7a7e3361fcf17ce4878ad1469f9a51ed0dd
arnauddchamps <36513985+arnauddchamps@users.noreply.github.com>
2019-11-11 13:40:30 +0100

    188 - Nos premières requêtes DQL

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -10,9 +11,16 @@ class AdminDashboardController extends AbstractController
     /**
      * @Route("/admin", name="admin_dashbard")
      */
-    public function index()
+    public function index(ObjectManager $manager)
     {
+
+        $users = $manager->createQuery('SELECT count(u) FROM App\Entity\User u')->getSingleScalarResult();
+        $ads = $manager->createQuery('SELECT count(a) FROM App\Entity\Ad a')->getSingleScalarResult();
+        $bookings = $manager->createQuery('SELECT count(b) FROM App\Entity\Booking b')->getSingleScalarResult();
+        $comments = $manager->createQuery('SELECT count(c) FROM App\Entity\Comment c')->getSingleScalarResult();
+
         return $this->render('admin/dashboard/index.html.twig', [
+            'stats' => compact('users', 'bookings', 'comments', 'ads')
         ]);
     }
 }
