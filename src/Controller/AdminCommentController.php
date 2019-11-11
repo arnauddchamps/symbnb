e1ab8890c39fb9605d26bf550b850453d86c5975
arnauddchamps <36513985+arnauddchamps@users.noreply.github.com>
2019-11-11 11:46:51 +0100

    166 - Solution  permettez à l'administrateur de supprimer un commentaire

---
+++ src/Controller/AdminCommentController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Form\AdminCommentType;
+use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminCommentController extends AbstractController
+{
+    /**
+     * @Route("/admin/comments", name="admin_comments_index")
+     */
+    public function index(CommentRepository $repo)
+    {
+        $comments = $repo->findAll();
+        
+        return $this->render('admin/comment/index.html.twig', [
+            'comments' => $comments,
+        ]);
+    }
+
+    /**
+     * Permet de modifier un commentaire
+     * 
+     * @Route("/admin/comments/{id}/edit", name="admin_comments_edit")
+     */
+    public function edit(Comment $comment, Request $request, ObjectManager $manager){
+
+        $form = $this->createForm(AdminCommentType::class, $comment);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Le commentaire a bien été enregistrée"
+            );
+        }
+
+        return $this->render('admin/comment/edit.html.twig',[
+            "comment" => $comment,
+            "form" => $form->createView()
+        ]);
+    }
+    
+    /**
+     * Permet de supprimer un commentaire
+     * 
+     * @Route("/admin/comments/{id}/delete", name="admin_comments_delete")
+     * 
+     */
+    public function delete(Comment $comment, ObjectManager $manager){
+
+        // if(count($ad->getBookings()) > 0){
+        //     $this->addFlash(
+        //         "warning",
+        //         "Vous ne pouvez pas supprimre l'annonce car elle possede déjà des réservations"
+        //     );
+        // } else {
+            $manager->remove($comment);
+            $manager->flush();
+    
+            $this->addFlash(
+                'success',
+                "Le commentaire a bien été supprimé"
+            );
+        // }
+
+
+        return $this->redirectToRoute('admin_comments_index');
+    }
+}
